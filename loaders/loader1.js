d3f32ad8577da2264c9dc327dd2bc85559968069
laibao101 <369632567@qq.com>
2019-06-17 17:09:16 +0800

    使用形式~

--- loaders/loader1.js
+++ loaders/loader1.js
@@ -2,7 +2,7 @@ function loader (source) {
     console.log('--- loader begin ----');
     console.log(source);
     console.log('--- loader end ----\n');
-    return source;
+    this.callback(null, source);
 }
 
 module.exports = loader;
